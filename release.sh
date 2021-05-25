0c192b1abc150bbdb1029ff061ba859ff1598031
vertion <35286924+vertiond@users.noreply.github.com>
2021-05-25 01:26:41 -0500

    Do not detach docker output

--- release.sh
+++ release.sh
@@ -5,4 +5,4 @@ mkdir output
 docker rm dogecoin-build
 
 echo Starting build
-docker run --detach --name dogecoin-build -v $PWD/../dogecoin-release:/root/dogecoin -v $PWD/output:/root/output dogecoinbuild ./build-all.sh
+docker run --name dogecoin-build -v $PWD/../dogecoin-release:/root/dogecoin -v $PWD/output:/root/output dogecoinbuild ./build-all.sh
